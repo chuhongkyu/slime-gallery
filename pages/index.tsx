62f9df61d189569abc8e488bf2b697defe3b4f4c
Mr.chu <chuhongkyu@gmail.com>
2024-01-14 21:04:01 +0900

    Add Slime Sticker

--- pages/index.tsx
+++ pages/index.tsx
@@ -52,9 +52,9 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
         )}
         <div className="columns-1 gap-4 sm:columns-2 xl:columns-3 2xl:columns-4">
           <div className="after:content relative mb-5 flex h-[629px] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white shadow-highlight after:pointer-events-none after:absolute after:inset-0 after:rounded-lg after:shadow-highlight lg:pt-0">
-            <div className="absolute inset-0 flex items-center justify-center opacity-20">
+            <div className="absolute inset-0 flex items-center justify-center opacity-40">
               <span className="flex max-h-full max-w-full items-center justify-center">
-                
+                <Image fill src='/main.webp'alt="angel"/>
               </span>
               <span className="absolute left-0 right-0 bottom-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black"></span>
             </div>
@@ -63,8 +63,8 @@ const Home: NextPage = ({ images }: { images: ImageProps[] }) => {
               Sticker Slime
             </h1>
             <p className="max-w-[40ch] text-white/75 sm:max-w-[32ch]">
-              Our incredible Next.js community got together in San Francisco for
-              our first ever in-person conference!
+              We've been loving sticker slime for a long time.
+              It started with ios, aos game sticker slime.
             </p>
             <a
               className="pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4"
